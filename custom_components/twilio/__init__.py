6b40de0e68135d5854c1428d14f95fc2bdf9bba0
Teagan glenn <that@teagantotally.rocks>
2026-02-14 12:56:02 -0700

    Update config flow

--- custom_components/twilio/__init__.py
+++ custom_components/twilio/__init__.py
@@ -100,6 +100,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     # Forward setup to platforms
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(async_options_updated))
 
     if not hass.data[DOMAIN].get(_SERVICES_REGISTERED):
         # Register services once for the integration domain.
@@ -202,6 +203,11 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             hass.data[DOMAIN].pop(_SERVICES_REGISTERED, None)
 
     return unload_ok
+
+
+async def async_options_updated(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload integration when options are updated."""
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async_remove_entry = config_entry_flow.webhook_async_remove_entry
